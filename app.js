a536ba8b1bbb9081d45be638d8d08ebc42589a5b
Andrew Havis <andrew@andrew-havis.co.uk>
2015-11-22 22:29:11 +0000

    Added random Flickr backgrounds

--- app.js
+++ app.js
@@ -15,14 +15,51 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// Get credentials from Cloud Foundry, or credentials.json if running locally
+if (!!appEnv.isLocal) {
+    console.log('Running locally');
+    var credentials = require('./credentials.json');
+}
+else {
+    console.log('Running on Bluemix');
+    var credentials = appEnv.getServices();
+}
+
+// Import Flickr API
+var Flickr = require('node-flickr');
+var flkrKeys = credentials.flickr;
+var flickr = new Flickr(flkrKeys);
+
+// Retrieve username to see if Flickr API is working
+flickr.get('people.getInfo', {"user_id": flkrKeys.user_id}, function(err, result) {
+    if (!err) {
+        console.log('Using photostream of Flickr user \'' + result.person.username._content + '\'');
+    }
+    else {
+        console.log('ERROR: Cannot access Flickr API\n' + err);
+    }
+});
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
 // get bower libraries
 app.use('/lib', express.static(__dirname + '/bower_components'));
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+// Flickr API
+app.use('/api/flickr', function(req, res) {
+    flickr.get('people.getPublicPhotos', {"user_id": flkrKeys.user_id, "extras": "url_l"}, function(err, result) {
+        if (!err) {
+            res.send(result);
+        }
+        else {
+            console.log('ERROR: Cannot access Flickr API\n' + err);
+        }
+    });
+});
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
