f77106857193a3656e226f7970ed5295eac6079f
Andrew Havis <andrew.havis@uk.ibm.com>
2015-11-25 01:09:24 +0000

    Changes to Gruntfile and some other general alterations

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,25 +6,71 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
+        
+        clean: {
+            tmp: ['./.tmp'],
+            dist: ['./dist', './public']
+        },
+        
+        mkdir: {
+            all: {
+              options: {
+                create: ['./.tmp', './public']
+              },
+            },
+          },
 
         copy: {
-            main: {
-                files: [
-                    {expand: true, cwd: './dev/', src: ['**'], dest: './public/'},
-                ],
-            },
             init: {
                 files: [
                     {expand: true, cwd: './theme/', src: ['**'], dest: './dev/'},
                 ],
+            },
+            main: {
+                files: [
+                    {expand: true, cwd: './.tmp/', src: ['**'], dest: './public/'},
+                    {expand: true, cwd: './public/concat/', src: ['**'], dest: './public/'}
+                ],
+            }
+        },
+        
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    './dev/css/landing-page.css': './dev/css/landing-page.scss'
+                }
+            }
+        },
+        
+        useminPrepare: {
+            html: './dev/index.html',
+            options: {
+                dest: './public'
+            }
+        },
+        
+        usemin: {
+            css: './public/css/{,*/}*.css',
+            js: './public/js/{,*/}*.js',
+            options: {
+                assetsDirs: ['./public']
+            }
+        },
+        
+        filerev: {
+            dist: {
+                src: ['./public/css/{,*/}*.css', './public/js/{,*/}*.js'],
             }
         }
-
+        
     });
 
     // Default task(s).
     grunt.registerTask('default', ['build']);
     grunt.registerTask('init', ['copy:init']);
-    grunt.registerTask('build', ['copy:main']);
+    grunt.registerTask('build', ['clean:dist', 'mkdir:all', 'sass:dist', 'useminPrepare', 'concat:generated', 'cssmin:generated', 'uglify:generated', 'copy:main', 'filerev', 'usemin']);
 
 };
--- app.js
+++ app.js
@@ -33,13 +33,32 @@ var Flickr = require('node-flickr');
 var flkrKeys = credentials.flickr;
 var flickr = new Flickr(flkrKeys);
 
+// Import Twitter API
+var Twitter = require('twitter');
+var twitter = new Twitter({
+    consumer_key: credentials.twitter.consumer_key,
+    consumer_secret: credentials.twitter.consumer_secret,
+    access_token_key: credentials.twitter.access_token,
+    access_token_secret: credentials.twitter.access_secret
+});
+
 // Retrieve username to see if Flickr API is working
 flickr.get('people.getInfo', {"user_id": flkrKeys.user_id}, function(err, result) {
     if (!err) {
         console.log('Using photostream of Flickr user \'' + result.person.username._content + '\'');
     }
     else {
-        console.log('ERROR: Cannot access Flickr API\n' + err);
+        console.error('ERROR: Cannot access Flickr API\n' + err);
+    }
+});
+
+// Also retrieve username from Twitter API
+twitter.get('users/show', {"user_id": credentials.twitter.user_id}, function(err, result) {
+    if (!err) {
+        console.log('Twitter username: @' + result.screen_name);
+    }
+    else {
+        console.error('ERROR: Cannot access Twitter API\n' + err);
     }
 });
 
@@ -51,12 +70,23 @@ app.use('/lib', express.static(__dirname + '/bower_components'));
 
 // Flickr API
 app.use('/api/flickr', function(req, res) {
-    flickr.get('people.getPublicPhotos', {"user_id": flkrKeys.user_id, "extras": "url_l"}, function(err, result) {
+    flickr.get('photosets.getPhotos', {"photoset_id": flkrKeys.photoset_id, "user_id": flkrKeys.user_id, "extras": "url_l", "media": "photos"}, function(err, result) {
         if (!err) {
             res.send(result);
         }
         else {
-            console.log('ERROR: Cannot access Flickr API\n' + err);
+            console.error('ERROR: Cannot access Flickr API\n' + err);
+        }
+    });
+});
+
+app.use('/api/twitter/me', function(req, res) {
+    twitter.get('users/show', {"user_id": credentials.twitter.user_id}, function(err, result) {
+        if (!err) {
+            res.send(result);
+        }
+        else {
+            console.error('ERROR: Cannot access Twitter API\n' + err);
         }
     });
 });
